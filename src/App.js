1c7ba33bb403e32178f766394c98cb6de6bf45f4
dilarakasak <dilarakasak7@gmail.com>
2023-07-28 15:11:32 +0300

    Finally

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-import Gorev from './Gorev';
 
 function App() {
   return (
-    <div>
+    <div className='arkaplan_resim'>
       <Header/>
-      <br/><br/>
-      <Gorev/>
     </div>
   );
 }
